224b442b6850079db8b1757bd6ef39e93d138e52
Tdatbkpro <trandat22062004@gmail.com>
2024-08-06 14:44:27 +0700

    front-end

---
+++ src/app/global.ts
@@ -0,0 +1,5 @@
+export const GlobalVariables = Object.freeze ({
+    URL_API : "https://datbk.click:2222/",
+    HOST_API : "https://datbk.click",
+    ACCESS_TOKEN: "Beaner " + localStorage.getItem("accesstoken")
+})
