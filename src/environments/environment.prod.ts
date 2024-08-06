bd9f9cc7519d27bc670f64d0196b77a10cb248ae
Trần Quốc Đạt <134699905+Tdatbkpro@users.noreply.github.com>
2024-08-06 21:09:08 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    apiUrl: 'https://datbkshoppapp.onrender.com/api'
+    apiUrl: 'https://datbkshoppapp.onrender.com/api/v1'
   };
   
