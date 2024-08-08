8a63128a9033c61b026ae606fe07ee19164301c0
Trần Quốc Đạt <134699905+Tdatbkpro@users.noreply.github.com>
2024-08-08 19:29:08 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // src/app/environments/environment.ts
 
-// export const environment = {
-//     production: false,
-//     apiBaseUrl: 'http://localhost:8088/api/v1'
-//   };
+export const environment = {
+    production: false,
+    apiBaseUrl: 'http://localhost:8088/api/v1'
+  };
   
