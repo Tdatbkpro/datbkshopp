c7594ba88dc3e31854b06f1882ae1de2b23863da
Trần Quốc Đạt <134699905+Tdatbkpro@users.noreply.github.com>
2024-08-08 20:49:31 +0700

    Update global.ts

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,5 +1,5 @@
 export const GlobalVariables = Object.freeze ({
-    URL_API : "https://datbk.click:2222/",
-    HOST_API : "https://datbk.click",
+    URL_API : "https://datbkshopbackend-aw1a.onrender.com",
+    HOST_API : "https://datbkshopbackend-aw1a.onrender.com",
     ACCESS_TOKEN: "Beaner " + localStorage.getItem("accesstoken")
 })
